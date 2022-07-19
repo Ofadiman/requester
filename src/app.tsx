4b944401cb91d2a17f865ed8c9d958cad12522e6
ofadiman <ofadiman@gmail.com>
2022-07-19 18:21:40 +0200

    feat: install material ui

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,17 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import Button from '@mui/material/Button'
+import { CssBaseline } from '@mui/material'
+import { Fragment } from 'react'
 
 function render() {
-  ReactDOM.render(<h2>Hello from React!</h2>, document.body);
+  ReactDOM.render(
+    <Fragment>
+      <CssBaseline />
+      <Button>Install Material UI!</Button>
+    </Fragment>,
+    document.body,
+  )
 }
 
-render();
+render()
