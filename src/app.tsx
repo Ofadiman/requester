6783ce7d64e30ffd86a0ec523ccd97c790a86d81
ofadiman <ofadiman@gmail.com>
2022-07-22 18:19:19 +0200

    feat: integrate redux toolkit query

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,17 @@
 import * as React from 'react'
-import * as ReactDOM from 'react-dom'
+import * as ReactDOMClient from 'react-dom/client'
 import { CssBaseline } from '@mui/material'
 import { Provider } from 'react-redux'
 import { store } from './store'
 import { CounterView } from './counter/counter.view'
+import { PokemonView } from './pokemon/pokemon.view'
 
-function render() {
-  ReactDOM.render(
-    <Provider store={store}>
-      <CssBaseline />
-      <CounterView />
-    </Provider>,
-    document.body,
-  )
-}
+const root = ReactDOMClient.createRoot(document.getElementById('react-app'))
 
-render()
+root.render(
+  <Provider store={store}>
+    <CssBaseline />
+    <CounterView />
+    <PokemonView />
+  </Provider>,
+)
--- src/store.ts
+++ src/store.ts
@@ -1,12 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { COUNTER_SLICE_NAME, counterSlice } from './counter/counter.slice'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+import { pokemonApi } from './pokemon/pokemon.api'
+import { setupListeners } from '@reduxjs/toolkit/dist/query'
 
 export const store = configureStore({
   reducer: {
     [COUNTER_SLICE_NAME]: counterSlice.reducer,
+    [pokemonApi.reducerPath]: pokemonApi.reducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(pokemonApi.middleware),
 })
+
+setupListeners(store.dispatch)
 
 type TypedDispatch = () => typeof store.dispatch
 export const useTypedDispatch: TypedDispatch = useDispatch
