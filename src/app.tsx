6f0d57448dbaa92549afe3a1c0d2b9bb35a65628
ofadiman <ofadiman@gmail.com>
2022-07-23 13:16:00 +0200

    feat: integrate react router

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { StrictMode } from 'react'
 import * as ReactDOMClient from 'react-dom/client'
 import { CssBaseline } from '@mui/material'
 import { Provider } from 'react-redux'
@@ -6,14 +7,29 @@ import { store } from './store'
 import { SettingsView } from './settings/settings.view'
 import { PokemonView } from './pokemon/pokemon.view'
 import { CounterView } from './counter/counter.view'
+import { HashRouter, Link, Route, Routes } from 'react-router-dom'
+import { HomeView } from './home/home.view'
 
 const root = ReactDOMClient.createRoot(document.getElementById('react-app'))
 
 root.render(
-  <Provider store={store}>
+  <StrictMode>
     <CssBaseline />
-    <CounterView />
-    <PokemonView />
-    <SettingsView />
-  </Provider>,
+    <Provider store={store}>
+      <HashRouter>
+        <nav>
+          <Link to={`/`}>Home view</Link>
+          <Link to={`/pokemon`}>Pokemon view</Link>
+          <Link to={`/settings`}>Settings view</Link>
+          <Link to={`/counter`}>Counter view</Link>
+        </nav>
+        <Routes>
+          <Route path={`/`} element={<HomeView />} />
+          <Route path={`/pokemon`} element={<PokemonView />} />
+          <Route path={`/settings`} element={<SettingsView />} />
+          <Route path={`/counter`} element={<CounterView />} />
+        </Routes>
+      </HashRouter>
+    </Provider>
+  </StrictMode>,
 )
---
+++ src/home/home.view.tsx
@@ -0,0 +1,6 @@
+import React, { FC } from 'react'
+import { Box } from '@mui/material'
+
+export const HomeView: FC = () => {
+  return <Box>Renders home view</Box>
+}
